b7bf4ba4daa699c2439c653a708f573b9b9c8cc4
MisterDova <maxchan0511@gmail.com>
2025-06-16 02:50:07 +0800

    edited gitignore

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -1,22 +1,22 @@
 return {
-	-- "beauwilliams/statusline.lua",
-	-- dependencies = {
-	-- 	"nvim-lua/lsp-status.nvim",
-	-- },
-	-- config = function()
-	-- 	require("statusline").setup({
-	-- 		match_colorscheme = true, -- Enable colorscheme inheritance (Default: false)
-	-- 		tabline = false, -- Enable the tabline (Default: true)
-	-- 		lsp_diagnostics = true, -- Enable Native LSP diagnostics (Default: true)
-	-- 		ale_diagnostics = true, -- Enable ALE diagnostics (Default: false)
-	-- 	})
-	-- end,
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		lazy = false,
-		config = function()
-			require("lualine").setup({})
-		end,
+	"beauwilliams/statusline.lua",
+	dependencies = {
+		"nvim-lua/lsp-status.nvim",
 	},
+	config = function()
+		require("statusline").setup({
+			match_colorscheme = true, -- Enable colorscheme inheritance (Default: false)
+			tabline = false, -- Enable the tabline (Default: true)
+			lsp_diagnostics = true, -- Enable Native LSP diagnostics (Default: true)
+			ale_diagnostics = true, -- Enable ALE diagnostics (Default: false)
+		})
+	end,
+	-- {
+	-- 	"nvim-lualine/lualine.nvim",
+	-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- 	lazy = false,
+	-- 	config = function()
+	-- 		require("lualine").setup({})
+	-- 	end,
+	-- },
 }
