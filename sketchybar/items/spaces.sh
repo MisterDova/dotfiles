04af06b20239b04c38331cf3cf35d4b62fed4005
MisterDova <maxchan0511@gmail.com>
2025-12-03 22:40:52 +0800

    3/12 update
    disable all sketchybar things except space manager and set to center

--- sketchybar/items/spaces.sh
+++ sketchybar/items/spaces.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
-# SPACE_ICONS=("" "󰊯" "󱋊" "" "" "" "" "" "" "")
-SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
+SPACE_ICONS=("" "󰊯" "󱋊" "" "" "" "" "" "" "")
+#SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
 for i in "${!SPACE_ICONS[@]}"
 do
   sid="$(($i+1))"
@@ -22,15 +22,16 @@ do
     script="$PLUGIN_DIR/space.sh"
     click_script="yabai -m space --focus $sid"
   )
-  sketchybar --add space space."$sid" left \
+  sketchybar --add space space."$sid" center\
              --set space."$sid" "${space[@]}" \
-                         paddiing_left=5 
+                         #padding_left=5 
 done
 
 sketchybar --add bracket spaces '/space\..*/' \
-           --set         spaces background.color=0xdd353535 \
+           #--set         spaces background.color=0xdd353535 \
+           --set         spaces background.color=0x00000000 \
                                 background.corner_radius=4  \
                                 background.height=28 \
                                 background.border_width=2.5 \
-                                background.border_color=0xffd6b4fc \
+                                #background.border_color=0xffd6b4fc \
                                 corner_radius=10 \
